30de000313c374b16ffa1d294b1069fb22125e77
Malaybhai11 <malay.raval11@gmail.com>
2025-08-24 20:06:33 +0530

    ADDING UNDERDEVELOPMENT SIGNS

---
+++ src/app/dashboard/delivery-tracking/page.tsx
@@ -0,0 +1,8 @@
+export default function UnderDevelopment() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-4xl font-bold mb-4">Feature Under Development</h1>
+        <p className="text-lg text-gray-600">I am working hard to bring you this feature.(But RDBMS SIR CAME IN) Stay tuned!</p>
+        </div>
+    );
+}
